850c648fb10bea25b34609030d645b7eab41818b
abhaythanak <abhaythanak@gmail.com>
2022-08-21 17:39:55 +0530

    week1

--- M2week1/app.js
+++ M2week1/app.js
@@ -1,11 +1,8 @@
 const getColor = () => {
     const randomNumber = Math.floor(Math.random() * 16777215);
-    const randomCode3 = "#" + randomNumber.toString(16);
-    const pallet1 = document.getElementById("pallet1");
-
+    const randomCode = "#" + randomNumber.toString(16);
     document.body.style.backgroundColor = randomCode;
-    document.getElementById("pallet1").innerText = randomCode;
-    
+    document.getElementById("hex").innerText = randomCode;
 }
 document.getElementById("butn").addEventListener(
     "click",
