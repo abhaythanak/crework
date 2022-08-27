b4fa10a36a36bf1ff6eb05d985757cf760d842a4
abhaythanak <abhaythanak@gmail.com>
2022-08-27 21:01:31 +0530

    week6

--- week6/app.js
+++ week6/app.js
@@ -2,8 +2,17 @@ let addButton = document.getElementById('addButton')
 let toDoContainer = document.getElementById('toContainer')
 let inputField = document.getElementById('inputField')
 
-addButton.addEventListener('click' ,() => {
+addButton.addEventListener('click' ,(event) => {
+    event.preventDefault();
+    //stops the form to submit
     let text = document.createElement('p')
     text.innerHTML = inputField.value;
     toDoContainer.appendChild(text);
+    inputField.value = ""
+    text.addEventListener('click',(event) =>{
+        text.style.textDecoration = "line-through";
+    })
+    text.addEventListener('dblclick',(event) =>{
+        toDoContainer.removeChild(text);
+    })
 })
