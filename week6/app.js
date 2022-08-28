bce8a6ec6c697f27a280c74ab56540e2c040824f
abhaythanak <abhaythanak@gmail.com>
2022-08-28 16:42:47 +0530

    week6

--- week6/app.js
+++ week6/app.js
@@ -1,18 +1,30 @@
-let addButton = document.getElementById('addButton')
-let toDoContainer = document.getElementById('toContainer')
-let inputField = document.getElementById('inputField')
-
-addButton.addEventListener('click' ,(event) => {
-    event.preventDefault();
+let addButton = document.getElementById("addButton");
+let toDoContainer = document.getElementById("toContainer");
+let inputField = document.getElementById("inputField");
+let id = 0;
+const taks = [];
+addButton.addEventListener("click", (event) => {
     //stops the form to submit
-    let text = document.createElement('p')
-    text.innerHTML = inputField.value;
+    let text = document.createElement("p");
+    text.id = id;
+    text.innerHTML = `<input type="checkbox" id="check">
+        <span>
+        ${inputField.value}
+        </span>
+        <button class="del" onclick="Delete(this,${id})">delete</button>`;
+    const check = document.getElementById("check");
     toDoContainer.appendChild(text);
-    inputField.value = ""
-    text.addEventListener('click',(event) =>{
+    id++;
+    inputField.value = "";
+    text.addEventListener("click", (event) => {
         text.style.textDecoration = "line-through";
-    })
-    text.addEventListener('dblclick',(event) =>{
-        toDoContainer.removeChild(text);
-    })
-})
+    });
+    // text.addEventListener('dblclick',(event) =>{
+    //     toDoContainer.removeChild(text);
+    // })
+});
+function Delete(currentEl, id) {
+    const toRemove = document.getElementById(id);
+    toRemove.remove();
+    console.log(currentEl);
+}
